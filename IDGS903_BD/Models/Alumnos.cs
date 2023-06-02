a7e2ac64aea30ca67563849676c657a0fb8b18ab
IDGS-803-20001470 <chaipsreyna@gmail.com>
2023-06-02 17:34:43 -0600

    Proyecto BD

---
+++ IDGS903_BD/Models/Alumnos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IDGS903_BD.Models
+{
+    public class Alumnos
+    {
+        public int Id {  get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Nombre { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Apellidos { get; set; }
+        public string Correo { get; set; }
+
+
+    }
+}
---
+++ IDGS903_BD/Models/AlumnosDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+
+namespace IDGS903_BD.Models
+{
+    public class AlumnosDbContext: DbContext
+    {
+        private static string conexion= "DefaultConnection" ;
+        public AlumnosDbContext() : base(conexion) { }
+        
+            public DbSet<Alumnos> Alumnos { get; set; }
+        
+    }
+}
